e664bfa8021949e078d5e5976962611817ac9b49
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-11-06 22:15:31 +0200

    removed errors from topbar

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,13 +1,13 @@
 import { Box, IconButton, useTheme } from '@mui/material';
 import { useContext } from 'react';
 import { ColorModeContext, tokens } from '../../theme';
-import InputBase from '@mui/material';
-import { LightModeOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
-import { DarkModeOutlinedIcon } from '@mui/icons-material/DarkModeOutlined';
-import { NotificationsModeOutlinedIcon } from '@mui/icons-material/NotificationsModeOutlined';
-import { SettingsOutlinedIcon } from '@mui/icons-material/SettingsOutlined';
-import { PersonOutlinedIcon } from '@mui/icons-material/PersonModeOutlined';
-import { SearchIcon } from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationsModeOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Topbar = () => {
   const theme = useTheme();
@@ -37,7 +37,7 @@ const Topbar = () => {
           ) : (
             <LightModeOutlinedIcon />
           )}
-          <LightModeOutlinedIcon />
+          {/* <LightModeOutlinedIcon /> */}
         </IconButton>
 
         <IconButton>
