6053bf4246575339b8585a8bbd417a486d49a290
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-11-21 21:40:13 +0200

    added geography page

---
+++ src/scenes/geography/index.jsx
@@ -0,0 +1,23 @@
+import { Box, colors } from '@mui/material';
+import Header from '../../components/Header';
+import GeographyChart from '../../components/GeographyChart';
+import { tokens } from '../../theme';
+import { useTheme } from '@mui/material';
+
+const Geography = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box mb="20px" ml="10px">
+      <Header title="Geography Chart" subtitle="Simple Geography Chart" />
+      <Box
+        height="75vh"
+        border={`1px solid ${colors.grey[100]}`}
+        borderRadius="4px"
+      >
+        <GeographyChart />
+      </Box>
+    </Box>
+  );
+};
+export default Geography;
