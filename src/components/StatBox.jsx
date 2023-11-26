7d2d4808c71663c009525873048b8de98771bcc8
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-11-26 06:25:43 +0200

    added row2 in dashboard or lineChart, transactions

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -22,24 +22,24 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
         <Box>
           <ProgressCircle progress={progress} />
         </Box>
-        <Box display="flex" justifyContent="space-between" mt="2px">
-          <Typography
-            variant="h6"
-            // fontWeight="bold"
-            sx={{ color: colors.greenAccent[500] }}
-          >
-            {subtitle}
-          </Typography>
+      </Box>
+      <Box display="flex" justifyContent="space-between" mt="2px">
+        <Typography
+          variant="h6"
+          // fontWeight="bold"
+          sx={{ color: colors.greenAccent[500] }}
+        >
+          {subtitle}
+        </Typography>
 
-          <Typography
-            variant="h5"
-            fontStyle="italic"
-            // fontWeight="bold"
-            sx={{ color: colors.greenAccent[600] }}
-          >
-            {increase}
-          </Typography>
-        </Box>
+        <Typography
+          variant="h5"
+          fontStyle="italic"
+          // fontWeight="bold"
+          sx={{ color: colors.greenAccent[600] }}
+        >
+          {increase}
+        </Typography>
       </Box>
     </Box>
   );
